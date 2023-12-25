253e376a943f1f14158cc0df50140fc98c69caa4
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-12-25 01:37:55 +0100

    Criando Search de Countries

--- script.js
+++ script.js
@@ -13,6 +13,36 @@ const themes = ()=>{
     });
 };
 
+const flags = (el)=>{
+        $template.querySelector('.flag-img').src = el.flags.svg;
+        $template.querySelector('.flag-img').alt = el.name;
+        $template.querySelector('.flag-name').innerHTML = el.name;
+        $template.querySelector('.flag-name').innerHTML = el.name;
+        $template.querySelector(".flag-population").textContent = el.population;
+        $template.querySelector(".flag-region").textContent = el.region;
+        $template.querySelector(".flag-capital").textContent = el.capital;
+
+        let $clone = d.importNode($template,true);
+        $fragmaneto.appendChild($clone);
+}
+
+const filtroRegion = (json)=>{
+    d.addEventListener("keyup",(e)=>{
+        $flagSection.innerHTML = "";
+        // console.log($inputSearch.value)
+       json.forEach(el =>{
+            let search = el.name.toLowerCase().match(new RegExp($inputSearch.value, "gi"));
+            if(e.key === 'Escape') $inputFilter.value = '';
+
+            if(el.region.toLowerCase().includes($inputFilter.value.toLowerCase())) flags(el);
+            
+            if(el.name.toLowerCase() === $inputSearch.value.toLowerCase()) flags(el)
+            
+        });
+        $flagSection.appendChild($fragmaneto);
+    });
+ };
+
 const dataPeticion = async () => {
     const error = d.createElement('h1');
    try{
@@ -25,28 +55,11 @@ const dataPeticion = async () => {
 
     let json = await res.json();
 
-    json.forEach(el => {
-        let {name,nativeName, population,region,subregion,capital,topLevelDomain,currencies,languages,alpha3Code
-        } = el;
+    json.forEach(el => flags(el));
 
-        $template.querySelector('.flag-img').src = el.flags.svg;
-        $template.querySelector('.flag-img').alt = el.name;
-        $template.querySelector('.flag-name').innerHTML = el.name;
-        $template.querySelector('.flag-name').innerHTML = el.name;
-        $template.querySelector(".flag-population").textContent = el.population;
-        $template.querySelector(".flag-region").textContent = el.region;
-        $template.querySelector(".flag-capital").textContent = el.capital;
+    filtroRegion(json);
 
-        let $clone = d.importNode($template,true);
-        $fragmaneto.appendChild($clone);
-    });
-
-    $flagSection.appendChild($fragmaneto)
-
-        console.log($fragmaneto)
-
-    console.log(json);
-    // return json;
+    $flagSection.appendChild($fragmaneto);
 
    }catch(err){
         console.error(err);
@@ -56,13 +69,7 @@ const dataPeticion = async () => {
    }
 };
 
-const filtroRegion = ()=>{
-   let flag = d.querySelectorAll(".flag");
-   flag.forEach((el)=> console.log(el.querySelectorAll(".flag-region").value))
-}
-d.addEventListener("keyup",(e)=>{
-   filtroRegion()
-})
+console.log(dataPeticion())
 d.addEventListener("DOMContentLoaded",()=>{
     dataPeticion();
     themes();
